d5e7891c7e950e1fe52fe7ec358518add2206e24
Fela Maslen <felamaslen@gmail.com>
2017-10-09 17:34:34 +0100

    Move songs list route to separate file

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const config = require('../../common/config');
 const Database = require('../../common/db');
 
+const { routeSongsList } = require('./songs-list');
+
 async function dbMiddleware(req, res, next) {
     req.db = await Database.dbConnect(config.dbUri);
 
@@ -15,34 +17,7 @@ function apiRoutes() {
 
     router.use(dbMiddleware);
 
-    router.get('/songs', async (req, res) => {
-        try {
-            const results = await req.db
-                .collection(config.collections.music)
-                .find({})
-                .toArray();
-
-            const songList = results
-                .map(result => [
-                    result._id,
-                    result.info.title,
-                    result.info.artist,
-                    result.info.album,
-                    result.info.year,
-                    Math.round(result.info.duration)
-                ]);
-
-            res.json(songList);
-        }
-        catch (err) {
-            res
-                .status(500)
-                .json({ status: 'Database error' });
-        }
-        finally {
-            req.db.close();
-        }
-    });
+    router.get('/songs', routeSongsList);
 
     router.get('/', (req, res) => {
         res
