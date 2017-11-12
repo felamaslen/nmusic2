ddb17c523eb33474501443aa8e361fcba249a3cb
Fela Maslen <felamaslen@gmail.com>
2017-11-12 19:03:59 +0000

    Format remote client like play queue item

--- src/containers/cloud-clients/cloud-client.js
+++ src/containers/cloud-clients/cloud-client.js
@@ -1,13 +1,27 @@
 import { Map as map } from 'immutable';
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 export default function CloudClient({ client }) {
-    return <li className="cloud-client">
-        <span className="origin">{client.get('origin')}</span>
+    const active = Boolean(client.get('currentSong'));
+
+    const className = classNames({
+        'cloud-client': true,
+        active
+    });
+
+    return <li className={className}>
+        <h4 className="origin">{client.get('origin')}</h4>
         <span className="info">
             <span className="title">
                 {client.getIn(['currentSong', 'title'])}
+            </span>
+            <span className="artist">
+                {client.getIn(['currentSong', 'artist'])}
+            </span>
+            <span className="album">
+                {client.getIn(['currentSong', 'album'])}
             </span>
         </span>
     </li>;
