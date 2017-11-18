d7d6d13afd72b2eaf36a20b85087ada5aadf3040
Fela Maslen <felamaslen@gmail.com>
2017-11-18 22:55:45 +0000

    Moved artwork into separate component

---
+++ src/components/artwork/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PureComponent from '../../ImmutableComponent';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+export default class Artwork extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: false
+        };
+    }
+    onLoad() {
+        this.setState({
+            loading: false
+        });
+    }
+    componentDidMount() {
+        if (this.props.src) {
+            this.setState({
+                loading: true
+            });
+        }
+    }
+    componentDidUpdate(prevProps) {
+        if (prevProps.src !== this.props.src) {
+            this.setState({
+                loading: true
+            });
+        }
+    }
+    render() {
+        const className = classNames({
+            artwork: true,
+            loading: this.state.loading
+        });
+
+        const onLoad = () => this.onLoad();
+
+        return <div className="artwork-outer">
+            <div className={className}>
+                <img src={this.props.src} onLoad={onLoad} onError={onLoad} />
+            </div>
+        </div>;
+    }
+}
+
+Artwork.propTypes = {
+    src: PropTypes.string
+};
+
--- src/containers/current-song-info/index.js
+++ src/containers/current-song-info/index.js
@@ -1,56 +1,19 @@
 import { connect } from 'react-redux';
 
 import React from 'react';
-import ImmutableComponent from '../../ImmutableComponent';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
-export class CurrentSongInfo extends ImmutableComponent {
-    constructor(props) {
-        super(props);
+import Artwork from '../../components/artwork';
 
-        this.state = {
-            artworkLoading: false
-        };
-    }
-    onArtworkLoad() {
-        this.setState({
-            artworkLoading: false
-        });
-    }
-    componentDidMount() {
-        if (this.props.artworkSrc) {
-            this.setState({
-                artworkLoading: true
-            });
-        }
-    }
-    componentDidUpdate(prevProps) {
-        if (prevProps.artworkSrc !== this.props.artworkSrc) {
-            this.setState({
-                artworkLoading: true
-            });
-        }
-    }
-    render() {
-        const artworkClasses = classNames({
-            artwork: true,
-            loading: this.state.artworkLoading
-        });
-
-        const onArtworkLoad = () => this.onArtworkLoad();
-
-        return <div className="current-song-info-outer">
-            <span className={artworkClasses}>
-                <img src={this.props.artworkSrc} onLoad={onArtworkLoad} onError={onArtworkLoad} />
-            </span>
-            <span className="info">
-                <span className="title">{this.props.title}</span>
-                <span className="artist">{this.props.artist}</span>
-                <span className="album">{this.props.album}</span>
-            </span>
-        </div>;
-    }
+export function CurrentSongInfo({ title, artist, album, artworkSrc }) {
+    return <div className="current-song-info-outer">
+        <Artwork src={artworkSrc} />
+        <span className="info">
+            <span className="title">{title}</span>
+            <span className="artist">{artist}</span>
+            <span className="album">{album}</span>
+        </span>
+    </div>;
 }
 
 CurrentSongInfo.propTypes = {
