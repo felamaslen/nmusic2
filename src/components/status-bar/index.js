c0da4a2b9841898dcab12afbad0cb861bb5743bd
Fela Maslen <felamaslen@gmail.com>
2017-11-24 23:52:28 +0000

    Basic shuffle toggle button

--- src/components/status-bar/index.js
+++ src/components/status-bar/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import ShuffleToggleButton from '../../containers/shuffle-toggle-button';
+
 export default function StatusBar() {
     return <div className="status-bar-outer">
-        {'Status bar'}
+        <ShuffleToggleButton />
     </div>;
 }
 
