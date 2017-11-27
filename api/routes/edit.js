58af3a67ab2ec2c3454f5b92215ebdc605e8ca44
Fela Maslen <felamaslen@gmail.com>
2017-11-27 22:49:27 +0000

    API route to update song info

---
+++ api/routes/edit.js
@@ -0,0 +1,42 @@
+const joi = require('joi');
+const { ObjectID } = require('mongodb');
+
+async function routeEdit(req, res) {
+    const schema = joi.object().keys({
+        track: joi.number(),
+        title: joi.string(),
+        artist: joi.string(),
+        album: joi.string()
+    });
+
+    const { error, value: fields } = joi.validate(req.body, schema);
+
+    if (error || Object.keys(fields).length === 0) {
+        return res.status(400)
+            .json({ error: true, status: 'Bad data input' });
+    }
+
+    const _id = new ObjectID(req.params.id);
+
+    const fieldsDeep = Object.keys(fields)
+        .reduce((fieldsObj, key) => ({ ...fieldsObj, [`info.${key}`]: fields[key] }), {});
+
+    try {
+        await req.db.collection('music')
+            .updateOne({ _id }, { $set: fieldsDeep });
+
+        res.json({ success: true });
+    }
+    catch (err) {
+        res.status(500)
+            .json({ error: true, status: 'Server error' });
+    }
+    finally {
+        req.db.close();
+    }
+}
+
+module.exports = {
+    routeEdit
+};
+
