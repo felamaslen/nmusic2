6c3bc49d5afb89e325492d5cc25a1c4ed521c275
Fela Maslen <felamaslen@gmail.com>
2017-11-27 22:31:16 +0000

    Combine wds into dev server

--- webpack.config.js
+++ webpack.config.js
@@ -9,23 +9,13 @@ if (process.env.DOTENV_INJECT === 'true' || process.env.NODE_ENV !== 'production
     dotenv.config();
 }
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const ExtractNormalCSS = new ExtractTextPlugin({
-    filename: 'css/style.css',
-    allChunks: true
-});
-const ExtractFontsCSS = new ExtractTextPlugin({
-    filename: 'css/fonts.css',
-    allChunks: true
-});
 
 const webpackConfigDevelopment = require('./webpack/conf.dev');
 const webpackConfigProduction = require('./webpack/conf.prod');
 
 function webpackConfig() {
     if (process.env.NODE_ENV === 'production') {
-        return webpackConfigProduction(ExtractNormalCSS, ExtractFontsCSS);
+        return webpackConfigProduction();
     }
 
     return webpackConfigDevelopment();
--- webpack/conf.prod.js
+++ webpack/conf.prod.js
@@ -1,15 +1,16 @@
 const webpack = require('webpack');
+const path = require('path');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const sassLoader = require('./sass-loader');
 
 const webpackConfig = require('./conf.common');
-const moduleConfigProd = require('./module.prod');
 
 const { version } = require('../package.json');
 
 const envInjectVars = [
-    'WEB_URI',
-    'SKIP_VISUALISER'
+    'WEB_URI'
 ];
 
 const injectedEnvVars = {
@@ -21,12 +22,25 @@ const injectedEnvVars = {
         return vars;
 
     }, {})
-}
+};
 
-module.exports = (...args) => ({
+const ExtractNormalCSS = new ExtractTextPlugin({
+    filename: 'css/style.css',
+    allChunks: true
+});
+const ExtractFontsCSS = new ExtractTextPlugin({
+    filename: 'css/fonts.css',
+    allChunks: true
+});
+
+module.exports = () => ({
     ...webpackConfig,
+    output: {
+        path: path.join(__dirname, '../static'),
+        filename: 'js/bundle.js'
+    },
     plugins: [
-        ...webpackConfig.plugins(...args),
+        ...webpackConfig.plugins,
         new webpack.DefinePlugin({
             'process.env': injectedEnvVars
         }),
@@ -49,15 +63,35 @@ module.exports = (...args) => ({
                 discardComments: { removeAll: true }
             }
         }),
+        ExtractNormalCSS,
+        ExtractFontsCSS,
         new HtmlWebpackPlugin({
             version,
-            analytics: injectedEnvVars.NO_ANALYTICS !== 'true',
-            externalStyles: true,
-            inject: false,
             template: 'src/templates/index.ejs'
         })
     ],
-    module: moduleConfigProd(...args),
+    module: {
+        ...webpackConfig.module,
+        loaders: [
+            ...webpackConfig.module.loaders,
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                exclude: /node_modules/,
+                loaders: `strip-loader?${JSON.stringify({ env: ['DEV'] })}!eslint-loader`
+            },
+            {
+                test: /\.scss$/,
+                exclude: [/fonts\.scss$/, /node_modules/],
+                loaders: ExtractNormalCSS.extract(sassLoader())
+            },
+            {
+                test: /fonts\.scss$/,
+                exclude: /node_modules/,
+                loaders: ExtractFontsCSS.extract('css-loader!sass-loader')
+            }
+        ]
+    },
     bail: true
 });
 
