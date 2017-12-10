6645118353d1e4fa1f989d87e498e4fed402c70b
Fela Maslen <felamaslen@gmail.com>
2017-12-10 19:40:15 +0000

    Split audio player reducer into separate files

--- src/reducers/audio-player.reducer.js
+++ src/reducers/audio-player.reducer.js
@@ -1,18 +1,6 @@
 import { List as list, Map as map } from 'immutable';
-
 import * as misc from '../constants/misc';
-
-const resetPlayerTimes = state => state
-    .setIn(['player', 'seekTime'], 0)
-    .setIn(['player', 'playTime'], 0)
-    .setIn(['player', 'dragTime'], null);
-
-const encodeArtistAlbum = (artist, album) => Buffer
-    .from([artist, album]
-        .map(item => encodeURIComponent(item))
-        .join('/')
-    )
-    .toString('base64');
+import { changeTrack, nextTrackShuffle } from './audio-track-queue.reducer';
 
 const PLAY_ICON = '\u25b6';
 const PAUSE_ICON = '\u23f8';
@@ -24,6 +12,18 @@ const titleStatusIcon = playing => {
 
     return PAUSE_ICON;
 };
+
+export const resetPlayerTimes = state => state
+    .setIn(['player', 'seekTime'], 0)
+    .setIn(['player', 'playTime'], 0)
+    .setIn(['player', 'dragTime'], null);
+
+const encodeArtistAlbum = (artist, album) => Buffer
+    .from([artist, album]
+        .map(item => encodeURIComponent(item))
+        .join('/')
+    )
+    .toString('base64');
 
 export const getArtworkSrc = song => `${misc.API_PREFIX}/artwork/${encodeArtistAlbum(
     song.get('artist') || '', song.get('album') || ''
@@ -87,164 +87,6 @@ export const audioStop = state => resetPlayerTimes(state)
     .setIn(['player', 'bufferedRanges'], list.of())
     .setIn(['artwork', 'src'], null);
 
-const goToSongStart = state => resetPlayerTimes(state)
-    .setIn(['player', 'seekTime'], -1);
-
-function getNextQueue(queue, queueActive) {
-    if (queueActive > -1) {
-        return queue.delete(queueActive);
-    }
-
-    return queue;
-}
-
-function nextTrackShuffle(
-    state,
-    songs = state.getIn(['songList', 'songs']),
-    currentId = state.getIn(['player', 'current'])
-) {
-    const invalidIds = state
-        .getIn(['songList', 'shuffledIds'])
-        .push(currentId);
-
-    const allIds = songs.map(song => song.get('id'));
-
-    let validIds = allIds.filterNot(id => invalidIds.includes(id));
-    if (!validIds.size) {
-        if (state.getIn(['player', 'repeat']) !== misc.REPEAT_LIST) {
-            return audioStop(state)
-                .setIn(['songList', 'shuffledIds'], list.of());
-        }
-
-        validIds = allIds;
-    }
-
-    const nextId = validIds.get(Math.floor(Math.random() * validIds.size));
-    const nextSong = songs.find(song => song.get('id') === nextId);
-
-    return loadAudioFile(state, nextSong)
-        .setIn(['songList', 'shuffledIds'], state
-            .getIn(['songList', 'shuffledIds'])
-            .push(nextId)
-        );
-}
-
-function nextTrackFromNone(state, queue, songs) {
-    const song = queue.size
-        ? queue.first()
-        : songs.first();
-
-    return loadAudioFile(state, song);
-}
-
-function nextTrackOnQueue(state, queue, queueActive) {
-    const queueActiveId = queue.getIn([queueActive, 'id']);
-    const newQueue = queue.filterNot(item => item.get('id') === queueActiveId);
-
-    return loadAudioFile(state, newQueue.first())
-        .setIn(['queue', 'songs'], newQueue)
-        .setIn(['queue', 'active'], 0);
-}
-
-function nextTrack(state, currentId, currentListIndex, songs, queue, queueActive) {
-    const shuffle = state.getIn(['player', 'shuffle']) === misc.SHUFFLE_ALL;
-    if (shuffle && !queue.size) {
-        return nextTrackShuffle(state, songs, currentListIndex);
-    }
-
-    if (!currentId) {
-        return nextTrackFromNone(state, queue, songs);
-    }
-
-    const repeatCurrentTrack = state.getIn(['player', 'repeat']) === misc.REPEAT_TRACK;
-    if (repeatCurrentTrack) {
-        return goToSongStart(state);
-    }
-
-    const startQueue = queue.size > 0 && queueActive === -1;
-    if (startQueue) {
-        return loadAudioFile(state, queue.first())
-            .setIn(['queue', 'active'], 0);
-    }
-
-    const continueQueue = queueActive > 0 || (queue.size > queueActive + 1 && queueActive > -1);
-    if (continueQueue) {
-        return nextTrackOnQueue(state, queue, queueActive);
-    }
-
-    const nextState = state.setIn(['queue', 'songs'], getNextQueue(queue, queueActive));
-
-    const songInList = currentListIndex !== -1;
-    if (songInList) {
-        const nextSongExists = songs.size > currentListIndex + 1;
-        if (nextSongExists) {
-            return loadAudioFile(nextState, songs.get(currentListIndex + 1), false);
-        }
-
-        if (songs.size > 0) {
-            const repeatList = state.getIn(['player', 'repeat']) === misc.REPEAT_LIST;
-
-            if (repeatList) {
-                return loadAudioFile(nextState, songs.first());
-            }
-        }
-    }
-
-    return audioStop(nextState);
-}
-
-function previousTrack(state, currentId, currentListIndex, songs, queue, queueActive) {
-    const currentPlayTime = state.getIn(['player', 'playTime']);
-
-    const goToStart = currentPlayTime > misc.REWIND_START_TIME;
-    if (goToStart) {
-        return goToSongStart(state);
-    }
-
-    const queueItemExists = queueActive > 0;
-    if (queueItemExists) {
-        return loadAudioFile(state, queue.get(queueActive - 1))
-            .setIn(['queue', 'active'], queueActive - 1);
-    }
-
-    const playLastListItem = songs.size > 0 && queueActive === 0;
-    if (playLastListItem) {
-        return loadAudioFile(state, songs.last())
-            .setIn(['queue', 'active'], -1);
-    }
-
-    const listItemExists = currentListIndex > 0;
-    if (listItemExists) {
-        return loadAudioFile(state, songs.get(currentListIndex - 1))
-            .setIn(['player', 'paused'], state.getIn(['player', 'paused']));
-    }
-
-    return audioStop(state);
-}
-
-export function changeTrack(state, direction) {
-    const songs = state.getIn(['songList', 'songs']);
-    const queue = state.getIn(['queue', 'songs']);
-    const queueActive = state.getIn(['queue', 'active']);
-
-    if (!songs.size && !queue.size) {
-        return audioStop(state);
-    }
-
-    const currentId = state.getIn(['player', 'current']);
-    const currentListIndex = songs.findIndex(song => song.get('id') === currentId);
-
-    if (direction > 0) {
-        return nextTrack(state, currentId, currentListIndex, songs, queue, queueActive);
-    }
-
-    if (direction < 0) {
-        return previousTrack(state, currentId, currentListIndex, songs, queue, queueActive);
-    }
-
-    return state;
-}
-
 export function handleAudioEnded(state) {
     return changeTrack(state, 1);
 }
@@ -263,7 +105,7 @@ export function setModeShuffle(state, status) {
     return state.setIn(['player', 'shuffle'], status);
 }
 
-function playFirstSong(state) {
+export function playFirstSong(state) {
     if (state.getIn(['queue', 'songs']).size > 0) {
         return loadAudioFile(state, state.getIn(['queue', 'songs']).first())
             .setIn(['queue', 'active'], 0);
@@ -308,73 +150,4 @@ export function playPauseAudio(state) {
         .setIn(['player', 'paused'], paused)
         .setIn(['cloud', 'localState', 'paused'], paused);
 }
-
-function rootOffsetLeft(elem) {
-    if (elem.offsetParent) {
-        return elem.offsetLeft + rootOffsetLeft(elem.offsetParent);
-    }
-
-    return elem.offsetLeft;
-}
-
-function getClickedPlayTime(state, { clientX, target }) {
-    const trough = target.parentNode.parentNode;
-
-    const progress = (clientX - rootOffsetLeft(trough)) / trough.offsetWidth;
-
-    if (isNaN(progress)) {
-        return state.getIn(['player', 'seekTime']);
-    }
-
-    const duration = state.getIn(['player', 'duration']);
-
-    return duration * progress;
-}
-
-const audioSeekRaw = (state, newTime) => state
-    .setIn(['player', 'dragTime'], null)
-    .setIn(['player', 'seekTime'], newTime)
-    .setIn(['player', 'playTime'], newTime);
-
-export function audioSeek(state, { raw, dragging, ...evt }) {
-    if (raw) {
-        return audioSeekRaw(state, evt.newTime);
-    }
-
-    const newTime = getClickedPlayTime(state, evt);
-
-    if (dragging) {
-        return state.setIn(['player', 'dragTime'], newTime);
-    }
-
-    return audioSeekRaw(state, newTime);
-}
-
-function getBufferedRangesAsProps(buffered, duration) {
-    if (!(duration && buffered && buffered.length)) {
-        return list.of();
-    }
-
-    return list(new Array(buffered.length).fill(0))
-        .map((item, key) => ({
-            left: `${buffered.start(key) / duration * 100}%`,
-            width: `${(buffered.end(key) - buffered.start(key)) / duration * 100}%`
-        }));
-}
-
-export const setAudioDuration = (state, duration) => state
-    .setIn(['player', 'buffered'], getBufferedRangesAsProps(
-        state.getIn(['player', 'bufferedRangesRaw']), duration
-    ))
-    .setIn(['player', 'duration'], duration);
-
-export const audioProgressBuffer = (state, { buffered, duration }) => state
-    .setIn(['player', 'bufferdRangesRaw'], buffered)
-    .setIn(['player', 'bufferedRanges'], getBufferedRangesAsProps(buffered, duration));
-
-export const audioTimeUpdate = (state, time) => state
-    .setIn(['player', 'playTime'], time);
-
-export const updateAudioSource = (state, audioSource) => state
-    .setIn(['player', 'audioSource'], audioSource);
 
